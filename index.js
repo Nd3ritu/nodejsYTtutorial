bc602bd2bb65c7e70a3e5bc0b3349875391b81d8
Nd3ritu <nderitumw17@gmail.com>
2024-09-11 13:54:34 +0300

    append file

--- index.js
+++ index.js
@@ -4,16 +4,23 @@
 const fs  =require('fs')
 const path = require('path')
 
-
+//Read file
 fs.readFile(path.join(__dirname, 'Files', 'starter.txt'), (err, data) => {
     if (err) throw err;
     console.log(data.toString())
     
 })
 
+
+//Write file
 fs.writeFile(path.join(__dirname, 'Files', 'reply.txt'), 'Nice to meet you', (err) => {
     if(err)  throw err;
     console.log('Write complete')
+})
+
+//Appendfile
+fs.appendFile(path.join(__dirname, 'Files', 'test.txt'), "testing text", () => {
+    console.log("Append complete")
 })
 
 
