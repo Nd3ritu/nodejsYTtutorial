76d640ec50bd23609315bdd740bc2336d8cf1fe3
Nd3ritu <nderitumw17@gmail.com>
2024-09-11 12:52:49 +0300

    import files

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 
 
+const {multiply, divide, subtract, add} = require('./math');
+console.log(multiply(6,6))
+console.log(add(5,6))
+console.log(divide(60,6))
+console.log(subtract(6,6))
+
+
+
+
 
 // const os = require('os');
 // console.log(os.type())
@@ -8,4 +17,4 @@
 // console.log(os.homedir())
 
 // console.log(__dirname)
-console.log(__filename)
+// console.log(__filename)
