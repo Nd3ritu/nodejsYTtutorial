57eb9938d91b06a28318339b02c8416ec23e9f7b
Nd3ritu <nderitumw17@gmail.com>
2024-09-12 12:03:35 +0300

    nodemon , uuid and date-fns installed

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 
-//import fs common core module 
+// //import fs common core module 
 
-const fs  =require('fs')
-const path = require('path')
+// const fs  =require('fs')
+// const path = require('path')
 
-//Read file
-fs.readFile(path.join(__dirname, 'Files', 'starter.txt'), (err, data) => {
-    if (err) throw err;
-    console.log(data.toString())
+// //Read file
+// fs.readFile(path.join(__dirname, 'Files', 'starter.txt'), (err, data) => {
+//     if (err) throw err;
+//     console.log(data.toString())
     
-})
+// })
 
 
 //Write file
-fs.writeFile(path.join(__dirname, 'Files', 'reply.txt'), 'Nice to meet you', (err) => {
-    if(err)  throw err;
-    console.log('Write complete')
-})
+// fs.writeFile(path.join(__dirname, 'Files', 'reply.txt'), 'Nice to meet you', (err) => {
+//     if(err)  throw err;
+//     console.log('Write complete')
+// })
 
 //Appendfile
-fs.appendFile(path.join(__dirname, 'Files', 'test.txt'), "testing text", () => {
-    console.log("Append complete")
-})
+// fs.appendFile(path.join(__dirname, 'Files', 'test.txt'), "testing text", () => {
+//     console.log("Update complete")
+// })
 
 
 // console.log(__dirname)
 // console.log(__filename)
+
+
+//date package
+const {format} = require('date-fns');
+const {v4 : uuid} = require('uuid') // generate different ID's
+
+console.log(format(new Date(),' yyyyMMdd\tHH:mm:ss'))
+console.log(uuid())
+console.log(uuid())
