5d985278aa39808e7df0d7c1419b7f24dda45814
Nd3ritu <nderitumw17@gmail.com>
2024-09-24 14:11:10 +0300

    read me file

--- express/server.js
+++ express/server.js
@@ -7,6 +7,16 @@ app.get('/', (req,res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));  //serve a file 
 })
 
+app.get('/newpage.html', (req,res) => {
+  res.sendFile(path.join(__dirname, 'views' , 'newpage.html'))
+
+
+})
+
+
+app.get('/oldpage(.html)?', (rq,res) => {
+  res.redirect(301, '/newpage.html')
+})
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT} `)) 
